687736b57c203520981c69f991101dacf412a774
ydfdas1f546g1df <niklauskuelling@gmx.ch>
2025-05-01 16:11:51 +0200

    Fix

--- run.sh
+++ run.sh
@@ -2,8 +2,15 @@
 set -e
 
 # Install dependencies
-echo "[*] Installing python-pip..."
-pacman -Sy --noconfirm python-archinstall git
+pacman -Sy --noconfirm python python-pip git
+
+echo "[*] Creating virtual environment..."
+python -m venv .venv
+source .venv/bin/activate
+
+echo "[*] Installing locked archinstall version..."
+pip install archinstall==2.8.3
+
 
 # Run the application
 echo "[*] Running base installer..."
