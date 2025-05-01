fcd4c082e43f6cda35c2f925e66d13412b2f6eae
ydfdas1f546g1df <niklauskuelling@gmx.ch>
2025-05-01 16:26:48 +0200

    Fix

--- run.sh
+++ run.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 set -e
 
+# Check if enough free ram is available
+if [ "$(free -m | awk '/^Mem:/{print $4}')" -lt 2048 ]; then
+    echo "Not enough free RAM available. At least 2GB is required."
+    exit 1
+fi
+
+# Make root directory bigger
+mkdir /tmp/newroot
+mount -t tmpfs -o size=1G tmpfs /tmp/newroot
+rsync -aXS / /tmp/newroot
+mount --bind /tmp/newroot /
+
+
 # Install dependencies
 pacman -Sy --noconfirm python python-pip git
-pacman -Sy --noconfirm pkgconf gcc make 
+pacman -Sy --noconfirm pkgconf gcc make
 
 echo "[*] Creating virtual environment..."
 python -m venv .venv
