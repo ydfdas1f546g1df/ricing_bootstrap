255a19062b957d6ed716e4b8e4f8b201d2fb1da8
ydfdas1f546g1df <niklauskuelling@gmx.ch>
2025-05-01 16:07:25 +0200

    Fix

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+set -e
 
 # Install dependencies
+echo "[*] Installing python-pip..."
+pacman -Sy --noconfirm python-pip git
+
+echo "[*] Installing python deps..."
 pip install -r requirements.txt
 
 # Run the application
+echo "[*] Running base installer..."
 python base_install/main.py
