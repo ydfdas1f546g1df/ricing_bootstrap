c339634d57dc211a79b1721266902ee3cedf0031
ydfdas1f546g1df <niklauskuelling@gmx.ch>
2025-05-01 16:14:56 +0200

    Fix

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Install dependencies
-pacman -Sy --noconfirm python python-pip git pkgconf
+pacman -Sy --noconfirm python python-pip git pkgconf gcc make 
 
 echo "[*] Creating virtual environment..."
 python -m venv .venv
