fad05d3bf420c77a932d2b94ca2c86c7e8185d12
ydfdas1f546g1df <niklauskuelling@gmx.ch>
2025-05-02 18:50:11 +0200

    Update archinstall version to 3.0.4

--- run.sh
+++ run.sh
@@ -15,13 +15,6 @@ fi
 echo "[*] Installing dependencies..."
 pacman -Sy --noconfirm git
 
-echo "[*] Creating virtual environment..."
-python -m venv .venv
-source .venv/bin/activate
-
-echo "[*] Installing locked archinstall version..."
-pip install archinstall==2.8.3
-
 
 # Run the application
 echo "[*] Running base installer..."
