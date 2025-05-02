0551926206d740252e858d6e7a6ae9ca4ebee834
ydfdas1f546g1df <niklauskuelling@gmx.ch>
2025-05-02 19:15:08 +0200

    Update archinstall version to 3.0.4

--- run.sh
+++ run.sh
@@ -5,20 +5,17 @@ echo "
 [*] Arch Linux Installer
 [*] This script will install Arch Linux with a custom configuration."
 
-# Check if enough free ram is available
-if [ "$(free -m | awk '/^Mem:/{print $4}')" -lt 2048 ]; then
-    echo "Not enough free RAM available. At least 2GB is required."
-    exit 1
-fi
 
 # Install dependencies
 echo "[*] Installing dependencies..."
 pacman -Sy --noconfirm git
 
+# Generate the Archinstall config
+echo "[*] Generating Archinstall config..."
+./base_install/generate_archinstall_config.sh
 
 # Run the application
 echo "[*] Running base installer..."
-python base_install/main.py
 
 echo "[*] Cloning repository..."
 git clone https://github.com/ydfdas1f546g1df/ricing_bootstrap.git /mnt/root/ricing_bootstrap
