004c4f2bec842ce35ab69b4a30fed1738ab830be
ydfdas1f546g1df <niklauskuelling@gmx.ch>
2025-05-03 10:57:45 +0200

    Role Firewall

--- playbook/run_ansible.sh
+++ playbook/run_ansible.sh
@@ -1,4 +1,11 @@
-pacman -S ansible
+#!/bin/bash
+
+if ! command -v ansible &> /dev/null; then
+  echo "[!] Installing Ansible..."
+  sudo pacman -Sy --noconfirm ansible
+else
+  echo "[*] Ansible already installed."
+fi
 
 ansible-galaxy install -r requirements.yml
 
